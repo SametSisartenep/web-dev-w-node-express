df73d47ede502801a0ce0bca74787ed5a10d9ab0
SametSisartenep <krashbuffer@gmail.com>
2014-12-17 10:48:14 +0100

    static directory and logo added.

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.get('/about', function ( req, res ) {
   res.render('about');
 });
 
+app.use(express.static(__dirname + '/public'));
+
 // custom 404 page
 app.use(function ( req, res ) {
   res.status(404);

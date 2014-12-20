1df3c83a9957a5bfd1fc3385fd0e1415c61d431b
SametSisartenep <krashbuffer@gmail.com>
2014-12-20 16:33:35 +0100

    fortune module created.

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ var express = require('express');
 var app = express();
 
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
-
-var fortunes = [
-  'Conquer your fears or they will conquer you.',
-  'Rivers need springs.',
-  'Do not fear what you don\'t know',
-  'You will have a pleasant surprise.',
-  'Whenever possible, keep it simple.'
-];
+var fortune = require('./lib/fortune');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -22,8 +15,7 @@ app.get('/', function ( req, res ) {
 });
 
 app.get('/about', function ( req, res ) {
-  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+  res.render('about', { fortune:  fortune.getFortune()});
 });
 
 app.use(express.static(__dirname + '/public'));

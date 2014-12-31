b98a7e3476958ba6e6b9535bd48aedd45fec02c5
SametSisartenep <krashbuffer@gmail.com>
2014-12-31 20:25:20 +0100

    Gruntfile for CI with Codeship created.

--- Gruntfile2.js
+++ Gruntfile2.js
@@ -2,8 +2,7 @@ module.exports = function ( grunt ) {
   // load plugins
   [
     'grunt-cafe-mocha',
-    'grunt-contrib-jshint',
-    'grunt-exec'
+    'grunt-contrib-jshint'
   ].forEach(function ( task ) {
     grunt.loadNpmTasks(task);
   });
@@ -16,12 +15,9 @@ module.exports = function ( grunt ) {
     jshint: {
       app: ['app.js', 'public/js/**/*.js', 'lib/**/*.js'],
       qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js']
-    },
-    exec: {
-      linkchecker: { cmd: 'linkchecker http://localhost:3000' }
     }
   });
 
   // register tasks
-  grunt.registerTask('default', ['cafemocha', 'jshint', 'exec']);
+  grunt.registerTask('default', ['cafemocha', 'jshint']);
 };

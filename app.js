47663d704b89e339aa74ca51def29465b04d0fb3
SametSisartenep <krashbuffer@gmail.com>
2015-01-01 19:49:20 +0100

    Weather widget added. Using partials with Handlebars!

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 var fortune = require('./lib/fortune');
+var weather = require('./lib/weather');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -13,6 +14,15 @@ app.set('port', process.env.PORT || 3000);
 app.use(function ( req, res, next ) {
   res.locals.showTests = app.get('env') !== 'production' &&
     req.query.test === '1';
+
+  next();
+});
+
+app.use(function ( req, res, next ) {
+  if (!res.locals.partials) {
+    res.locals.partials = {};
+  }
+  res.locals.partials.weather = weather.getWeatherData();
 
   next();
 });
--- qa/tests-unit.js
+++ qa/tests-unit.js
@@ -1,8 +1,16 @@
 var fortune = require('../lib/fortune');
+var weather = require('../lib/weather');
 var expect = require('chai').expect;
 
 suite('Fortune cookie tests', function () {
   test('getFortune() should return a fortune', function () {
     expect(typeof fortune.getFortune() === 'string');
+  });
+});
+
+suite('Weather info tests', function () {
+  test('getWeatherData() should return an object', function () {
+    var weatherData = weather.getWeatherData();
+    expect(typeof weatherData === 'object' && !Array.isArray(weatherData));
   });
 });

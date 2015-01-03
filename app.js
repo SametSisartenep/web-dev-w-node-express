f6efd33942e0b42f9b70f125024080f6c625f9a2
SametSisartenep <krashbuffer@gmail.com>
2015-01-03 15:23:20 +0100

    Form handling implemented with AJAX. Method: POST

--- app.js
+++ app.js
@@ -116,12 +116,24 @@ app.get('/newsletter', function ( req, res ) {
   res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
 
+/**
+
 app.post('/process', function ( req, res ) {
   console.log('Form (from querystring): ' + req.query.form);
   console.log('CSRF token (from hidden form field): ' + req.body._csrf);
   console.log('Name (from visible form field): ' + req.body.name);
   console.log('Email (from visible form field): ' + req.body.email);
   res.redirect(303, '/thank-you');
+});
+
+**/
+app.post('/process', function ( req, res ) {
+  if (req.xhr || req.accepts('json,html') === 'json') {
+    // if there were an error, we would send { error: 'error description' }
+    res.send({ success: true });
+  } else {
+    res.redirect(303, '/thank-you');
+  }
 });
 
 // custom 404 page
